f860aa375fdfeb193ab5046afc4fc48a2d41b962
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2022-10-19 14:43:09 +0100

    Visualised nodes and connections between src and dst

--- visualiser/index.py
+++ visualiser/index.py
@@ -1,18 +1,42 @@
+from matplotlib.pyplot import fill
 import pandas as pd
+import networkx as nx
 
-"""
-1. Read in contents of log_folder
-    1.1. Read conents of msg.csv
-    1.2. Read contents of operation.csv
-2. For each operation in operation.csv:
-    2.1. Get the message id
-    2.2. Find all messages with that id
-    2.3. Show the original src from the operation and all dst
-"""
+from pprint import pprint
+from rich.markdown import Markdown
+from rich import print
+from rich.console import Console
+from pyvis.network import Network
 
+console = Console(record = True)
+# ---------------------------------------------------------------------------- #
+#                                 File Reading                                 #
+# ---------------------------------------------------------------------------- #
+# ? Read contents of operation
 op_df = pd.read_csv('log_folder/operation.csv')
+# src | id | type | messages
+# ? Read contents of msg
 msg_df = pd.read_csv('log_folder/msg.csv')
+# dst | src | id | type | status
 
-for index, row in op_df.iterrows():
-    id = row["id"]
+nx_graph = nx.Graph()
+
+num_len = 6
+physics = True
+
+for index, row in msg_df.iterrows():
+    id = str(row["id"])[:num_len]
+    src = str(row["src"])[:num_len]
+    dst = str(row["dst"])[:num_len]
     
+    nx_graph.add_node(id, label=id, physics=physics)
+    nx_graph.add_edge(src, dst, physics=physics)
+    
+for index, row in op_df.iterrows():
+    id = str(row["id"])[:num_len]
+    nx_graph.add_node(id, label=id, color="green", physics=physics)
+    
+    
+nt = Network('720px', '1280px', notebook=True)
+nt.from_nx(nx_graph)
+nt.show("nx.html")
