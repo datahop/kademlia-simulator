52778fdc1dead4b7f47c4facacc39c79dcabd75a
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2022-10-26 12:18:01 +0100

    Updated and improved the visualiser to show multiple operations

--- visualiser/index.py
+++ visualiser/index.py
@@ -1,73 +1,70 @@
+from lib2to3.pytree import Node
 import matplotlib.pyplot as plt
 import pandas as pd
 
 from rich.console import Console
 
 console = Console(record = True)
-# ---------------------------------------------------------------------------- #
-#                                 File Reading                                 #
-# ---------------------------------------------------------------------------- #
-# ? Read contents of operation
-op_df = pd.read_csv('log_folder/operations.csv')
-# src | id | type | messages
-# ? Read contents of msg
-msg_df = pd.read_csv('log_folder/msg.csv')
-# dst | src | id | type | status
 
-num_len = 6
-physics = False
-image_width = 750
-image_height = 750
+with console.status("Visualising..."):
+    # ? Read contents of operation
+    op_df = pd.read_csv('log_folder/operations.csv')
+    # src | id | type | messages
+    # ? Read contents of msg
+    msg_df = pd.read_csv('log_folder/msg.csv')
+    # dst | src | id | type | status
 
-message_ids = set(msg_df["id"])
+    num_len = 6
+    physics = False
+    image_width = 750
+    image_height = 750
 
-for index, row in op_df.iterrows():
-    # ? Get the messages from the operation
-    messages = [int(x) for x in row["messages"].split("|")]
-    
-    # ? Get all messages used in the operation
-    operated_msg_df = (msg_df.loc[ msg_df["id"].isin(messages) ])
+    message_ids = set(msg_df["id"])
 
-# ? op_df stores all operation sources.
-# ? operated_msg_df stores all messages involved in that operation.
-    
-fig, axs = plt.subplots(len(op_df), figsize=(30, 30))
+    fig, ax = plt.subplots(figsize=(30, 30))
 
+    max_op_id = op_df.loc[op_df["src"].idxmax()]["id"]
+    min_op_id = op_df.loc[op_df["src"].idxmin()]["id"]
 
-biggest_node_id = operated_msg_df["dst"].max() if operated_msg_df["dst"].max() > operated_msg_df["src"].max() else operated_msg_df["src"].max()
-smallest_node_id = operated_msg_df["dst"].min() if operated_msg_df["dst"].min() < operated_msg_df["src"].min() else operated_msg_df["src"].min()
+    ax.set_ylim(ymin=min_op_id * 0.8, ymax= max_op_id * 1.2)
+    for index, row in op_df.iterrows():
+        op_id = row["id"]
+        op_src = row["src"]
 
-node_color = "#c1e7ff"
-node_label_color = "#004c6d"
-dot_size = 2000
-min = 0
-max = biggest_node_id * 1.2
+        message_ids = [int(x) for x in row["messages"].split("|")]
+        msg_dsts = list((msg_df.loc[ msg_df["id"].isin(message_ids) ])["dst"])
 
-for index, row in op_df.iterrows():
-    if len(op_df) == 1:
-        ax = axs
-    else:
-        ax = axs[index]
-
-    # ax.axhline(y=index, color=node_label_color)
-    ax.hlines(y=index, xmin=smallest_node_id, xmax=biggest_node_id, linewidth=5, color=node_color)
-    
-    ax.set_xlim(xmin=0, xmax=max)
-    ax.set_ylim(ymin=-5, ymax=5)
-    
-    ax.set_yticklabels([])
-    ax.set_xticklabels([])
-    ax.set_yticks([])
-    ax.set_xticks([])
+        # print(str(op_src) + ": ", end='')
+        # print(msg_dsts)
         
-    ax.scatter([int(x) for x in operated_msg_df["src"]], [index] * len(operated_msg_df), s=dot_size, color=node_color)
-    ax.scatter([int(x) for x in operated_msg_df["dst"]], [index] * len(operated_msg_df), s=dot_size, color=node_color)
-    
-    for src in op_df["src"]:
-        ax.annotate(str(src)[:5], (src, index), color=node_label_color, fontweight="bold", fontsize=30)
-    for src in operated_msg_df["src"]:
-        ax.annotate(str(src)[:5], (src, index), color=node_label_color, fontweight="bold", fontsize=30)
-    for dst in operated_msg_df["dst"]:
-        ax.annotate(str(dst)[:5], (dst, index), color=node_label_color, fontweight="bold", fontsize=30)
-            
-plt.savefig("output.png")
+        # ? op_id is the id of the op so that we can plot multiple operations by using the op_id on the y-axis
+        # ? op_src is the src node (node all the way on the left)
+        # ? msg_dsts is the list of nodes that have received from the op_src (nodes on the line in an ordered fashion)
+
+        min_node = op_src if min(msg_dsts) > op_src else min(msg_dsts)
+        max_node = max(msg_dsts)
+        node_color = "#c1e7ff"
+        node_label_color = "#004c6d"
+        dot_size = 2000
+        min_val = 0
+        max_val = max_node * 1.2
+        
+        ax.scatter([op_src], [op_id], s=dot_size, color="white", edgecolors=node_label_color)
+        ax.annotate(str(op_src)[:5], (op_src, op_id), color=node_label_color, fontweight="bold", fontsize=40)
+        
+        ax.scatter(msg_dsts, [op_id] * len(msg_dsts), s=dot_size, color="white", edgecolors=node_label_color)
+        
+        for dst in msg_dsts:
+            ax.annotate(str(dst)[:5], (dst, op_id), color=node_label_color, fontweight="bold", fontsize=40)
+        
+        ax.set_xscale("log")
+        ax.set_yticklabels([])
+        ax.set_xticklabels([])
+        ax.set_yticks([])
+        ax.set_xticks([])
+        ax.spines.right.set_visible(False)
+        ax.spines.left.set_visible(False)
+        ax.spines.top.set_visible(False)
+        ax.spines.bottom.set_visible(False)
+        
+        plt.savefig("output.png")
