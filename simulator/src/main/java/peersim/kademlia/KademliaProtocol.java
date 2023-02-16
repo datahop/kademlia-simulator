2200685a2742dd9cf35abc14892adcbf9c77b194
Sergi Rene <sergi@datahop.network>
2023-02-16 15:30:12 +0100

    get logger

--- simulator/src/main/java/peersim/kademlia/KademliaProtocol.java
+++ simulator/src/main/java/peersim/kademlia/KademliaProtocol.java
@@ -530,4 +530,8 @@ public class KademliaProtocol implements Cloneable, EDProtocol {
         });
     logger.addHandler(handler);
   }
+
+  public Logger getLogger() {
+    return this.logger;
+  }
 }
