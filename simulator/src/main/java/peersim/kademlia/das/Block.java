726a9347efcd7b2cf6f8be7fc0c4ce1685774384
OnurAscigil <o.ascigil@ucl.ac.uk>
2023-02-16 11:11:53 +0000

    region-based sample placement config and fixes

--- simulator/src/main/java/peersim/kademlia/das/Block.java
+++ simulator/src/main/java/peersim/kademlia/das/Block.java
@@ -3,22 +3,43 @@ package peersim.kademlia.das;
 import java.math.BigInteger;
 import java.util.Iterator;
 import peersim.core.CommonState;
+import peersim.core.Network;
+import peersim.kademlia.KademliaCommonConfig;
 
 public class Block implements Iterator<Sample> {
 
+  /** The square matrix of samples */
   private Sample[][] blockSamples;
 
+  /** Block identifier */
   private long blockId;
 
   private static long ID_GENERATOR = 0;
 
+  /** Row and column numbers used by the iterator */
   private int row, column;
 
+  /** block square matrix dimension (number of samples per row/column) */
   private int SIZE;
+
+  /** Maximum key in the keyspace of BITS length */
+  public static BigInteger MAX_KEY;
+
+  /** allow to call the service initializer only once */
+  private static boolean _ALREADY_INITIALISED = false;
+
+  /** gap between two samples in the keyspace */
+  public static BigInteger INTER_SAMPLE_GAP;
+
+  /** number of samples in a block */
+  private int numSamples;
 
   public Block() {
 
     SIZE = 512;
+    this.numSamples = this.SIZE * this.SIZE;
+    _init();
+
     this.blockId = (ID_GENERATOR++);
     blockSamples = new Sample[SIZE][SIZE];
     row = column = 0;
@@ -26,7 +47,7 @@ public class Block implements Iterator<Sample> {
 
       for (int j = 0; j < blockSamples[0].length; j++) {
 
-        blockSamples[i][j] = new Sample(blockId, i, j);
+        blockSamples[i][j] = new Sample(blockId, i, j, this);
       }
     }
   }
@@ -34,6 +55,9 @@ public class Block implements Iterator<Sample> {
   public Block(int size) {
 
     SIZE = size;
+    this.numSamples = this.SIZE * this.SIZE;
+    _init();
+
     this.blockId = (ID_GENERATOR++);
     blockSamples = new Sample[SIZE][SIZE];
     row = column = 0;
@@ -41,9 +65,24 @@ public class Block implements Iterator<Sample> {
 
       for (int j = 0; j < blockSamples[0].length; j++) {
 
-        blockSamples[i][j] = new Sample(blockId, i, j);
+        blockSamples[i][j] = new Sample(blockId, i, j, this);
       }
     }
+  }
+
+  /** Compute the radius of the region containing the desired number of copies of each sample */
+  public BigInteger computeRegionRadius(int numberOfCopiesPerSample) {
+
+    double totalSamples = ((1.0 * this.numSamples) / Network.size()) * numberOfCopiesPerSample;
+    if (totalSamples < 1.0) totalSamples = 1.0;
+    totalSamples = totalSamples / 2.0;
+
+    System.out.println("INTER_SAMPLE_GAP: " + INTER_SAMPLE_GAP.toString(2));
+
+    BigInteger reg_size = INTER_SAMPLE_GAP.multiply(BigInteger.valueOf(((long) totalSamples)));
+    System.out.println("Radius: " + reg_size.toString(2));
+
+    return reg_size;
   }
 
   public long getBlockId() {
@@ -95,5 +134,29 @@ public class Block implements Iterator<Sample> {
 
   public void initIterator() {
     column = row = 0;
+  }
+
+  public int getSize() {
+    return this.SIZE;
+  }
+
+  public int getNumSamples() {
+    return this.numSamples;
+  }
+
+  private void _init() {
+
+    // execute once
+    if (_ALREADY_INITIALISED) return;
+
+    MAX_KEY = BigInteger.ZERO.setBit(KademliaCommonConfig.BITS).subtract(BigInteger.ONE);
+
+    try {
+      INTER_SAMPLE_GAP = MAX_KEY.divide(BigInteger.valueOf(this.numSamples));
+    } catch (ArithmeticException e) {
+      e.printStackTrace();
+    }
+
+    _ALREADY_INITIALISED = true;
   }
 }
--- simulator/src/main/java/peersim/kademlia/das/Sample.java
+++ simulator/src/main/java/peersim/kademlia/das/Sample.java
@@ -4,26 +4,74 @@ import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import peersim.kademlia.KademliaCommonConfig;
 
 public class Sample {
 
+  /** Row and column numbers of a sample within a block */
   private int row, column;
+  /** The unique ID of the block that this sample belongs to */
   private long blockId;
+  /** The key of a sample in the DHT keyspace (after mapping) */
   private BigInteger id;
+  /** Block that this sample is part of */
+  private Block block;
 
-  public Sample(long blockId, int row, int column) {
+  /** Initialise a sample instance and map it to the keyspace */
+  public Sample(long blockId, int row, int column, Block b) {
 
-    try {
+    this.id = null;
+    this.block = b;
+    this.row = row;
+    this.column = column;
+    computeID();
+  }
 
-      MessageDigest digest = MessageDigest.getInstance("SHA-256");
+  /**
+   * Sample numbering to map each sample to an integer in the range 1 to SIZE*SIZE Samples are
+   * ordered by row
+   */
+  public int sampleNumberByRow() {
+    return this.row * this.block.getSize() + this.column;
+  }
 
-      String idName =
-          String.valueOf(blockId) + "_" + String.valueOf(row) + "x" + String.valueOf(column);
-      byte[] hash = digest.digest(idName.getBytes(StandardCharsets.UTF_8));
-      this.id = new BigInteger(1, hash);
-    } catch (NoSuchAlgorithmException e) {
-      e.printStackTrace();
+  /**
+   * Sample numbering to map each sample to an integer in the range 1 to SIZE*SIZE Samples are
+   * ordered by column
+   */
+  public int sampleNumberByColumn() {
+    return this.column * this.block.getSize() + this.row;
+  }
+
+  /** Map this sample to the DHT keyspace */
+  public void computeID() {
+    if (KademliaCommonConfig.MAPPING_FN == KademliaCommonConfig.SAMPLE_MAPPING_RANDOM) {
+      try {
+        String idName =
+            String.valueOf(blockId) + "_" + String.valueOf(row) + "x" + String.valueOf(column);
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(idName.getBytes(StandardCharsets.UTF_8));
+        this.id = new BigInteger(1, hash);
+      } catch (NoSuchAlgorithmException e) {
+        e.printStackTrace();
+      }
+    } else if (KademliaCommonConfig.MAPPING_FN
+        == KademliaCommonConfig.SAMPLE_MAPPING_REGION_BASED) {
+      this.id = Block.INTER_SAMPLE_GAP.multiply(BigInteger.valueOf(this.sampleNumberByRow()));
+
+    } else {
+      System.out.println("Error: invalid selection for sample mapping function");
+      System.exit(1);
     }
+  }
+
+  /** Given the peerID of a node, determine if this sample falls within the region of the node. */
+  public boolean isInRegion(BigInteger peerID, BigInteger radius) {
+    /** (peerID - radius) < this.id < (peerID + radius) */
+    if ((this.id.compareTo(peerID.subtract(radius)) == 1)
+        && (this.id.compareTo(peerID.add(radius)) == -1)) return true;
+
+    return false;
   }
 
   public int getRow() {
