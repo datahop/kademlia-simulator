a9f55d129517abcd44e5ac4b70279c39510e0a8d
sergi <sergi@srenevic.net>
2023-02-23 17:41:39 +0100

    search table for lookups

---
+++ simulator/src/main/java/peersim/kademlia/das/SearchTable.java
@@ -0,0 +1,35 @@
+package peersim.kademlia.das;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+// import java.util.Random;
+import peersim.core.CommonState;
+
+public class SearchTable {
+
+  private List<BigInteger> nodes;
+
+  public SearchTable() {
+
+    nodes = new ArrayList<>();
+  }
+
+  // add a neighbour to the correct k-bucket
+  public void addNode(BigInteger[] neighbours) {
+    nodes.addAll(Arrays.asList(neighbours));
+  }
+
+  public BigInteger getNode() {
+
+    BigInteger node = nodes.get(CommonState.r.nextInt(nodes.size()));
+    nodes.remove(node);
+    return node;
+  }
+
+  // remove a neighbour from the correct k-bucket
+  public void removeNeighbour(BigInteger node) {
+    nodes.remove(node);
+  }
+}
