c160bbc00b008df476447251a629e44374322e53
Sergi Rene <sergi@datahop.network>
2023-05-26 11:23:30 +0200

    clean code

---
+++ simulator/src/main/java/peersim/kademlia/das/DASDHTProtocol.java
@@ -0,0 +1,12 @@
+package peersim.kademlia.das;
+
+
+public class DASDHTProtocol extends DASProtocol {
+
+  protected static String prefix = null;
+
+  public DASDHTProtocol(String prefix) {
+    super(prefix);
+  }
+
+}
