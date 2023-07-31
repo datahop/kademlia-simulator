41adb6710344b00ec4018f375528cf9231ff9356
sergi <sergi@srenevic.net>
2023-07-31 13:30:09 +0200

    init commit new das classes

---
+++ simulator/src/main/java/peersim/kademlia/das/DASProtocolValidator.java
@@ -0,0 +1,49 @@
+package peersim.kademlia.das;
+
+import peersim.kademlia.Message;
+
+public class DASProtocolValidator extends DASProtocol {
+
+  protected static String prefix = null;
+
+  public DASProtocolValidator(String prefix) {
+    super(prefix);
+    EvilDASProtocol.prefix = prefix;
+  }
+
+  @Override
+  protected void handleGetSample(Message m, int myPid) {
+    /** Ignore sample request * */
+    logger.warning("Handle get sample - return nothing " + this);
+  }
+
+  @Override
+  protected void handleInitNewBlock(Message m, int myPid) {
+    logger.warning("Init block evil node - do nothing");
+  }
+
+  @Override
+  protected void handleInitGetSample(Message m, int myPid) {
+    logger.warning("Init block evil node - getting samples " + this);
+    // super.handleInitGetSample(m, myPid);
+  }
+
+  @Override
+  protected void handleGetSampleResponse(Message m, int myPid) {
+    logger.warning("Received sample evil node: do nothing");
+  }
+
+  /*public void processEvent(Node myNode, int myPid, Object event) {
+    logger.warning("Process event " + myPid + " " + this);
+  }*/
+  /**
+   * Replicate this object by returning an identical copy.<br>
+   * It is called by the initializer and do not fill any particular field.
+   *
+   * @return Object
+   */
+  public Object clone() {
+    EvilDASProtocol dolly = new EvilDASProtocol(EvilDASProtocol.prefix);
+    return dolly;
+  }
+}
