42ccde7621d43a90c865a6ee62f3e117ba0cdc16
OnurAscigil <o.ascigil@ucl.ac.uk>
2024-06-07 15:42:32 +0100

    adding missing protocols

---
+++ simulator/src/main/java/peersim/kademlia/das/DASProtocolEvilValidator.java
@@ -0,0 +1,52 @@
+package peersim.kademlia.das;
+
+import peersim.kademlia.Message;
+
+public class DASProtocolEvilValidator extends DASProtocolValidator {
+
+  protected static String prefix = null;
+
+  public DASProtocolEvilValidator(String prefix) {
+    super(prefix);
+    DASProtocolEvilValidator.prefix = prefix;
+    isEvil = true;
+    isValidator = true;
+    isBuilder = false;
+  }
+
+  @Override
+  protected void handleInitGetSample(Message m, int myPid) {
+    logger.warning("Init block evil node validator - getting samples " + this);
+    // super.handleInitGetSample(m, myPid);
+  }
+
+  /*protected void handleGetSample(Message m, int myPid) {
+    // kv is for storing the sample you have
+    logger.info("KV size " + kv.occupancy() + " from:" + m.src.getId() + " " + m.id);
+    // sample IDs that are requested in the message
+
+    Message response = new Message(Message.MSG_GET_SAMPLE_RESPONSE, new Sample[] {});
+    response.operationId = m.operationId;
+    response.dst = m.src;
+    response.src = this.kadProtocol.getKademliaNode();
+    response.ackId = m.id; // set ACK number
+    response.value = searchTable.getEvilNeighbours(KademliaCommonConfigDas.MAX_NODES_RETURNED);
+    sendMessage(response, m.src.getId(), myPid);
+  }*/
+
+  /*
+  public void setEvil(List<Node> evilIds) {
+    searchTable.setEvil(evilIds);
+  }*/
+
+  /**
+   * Replicate this object by returning an identical copy.<br>
+   * It is called by the initializer and do not fill any particular field.
+   *
+   * @return Object
+   */
+  public Object clone() {
+    DASProtocolEvilValidator dolly = new DASProtocolEvilValidator(DASProtocolEvilValidator.prefix);
+    return dolly;
+  }
+}
