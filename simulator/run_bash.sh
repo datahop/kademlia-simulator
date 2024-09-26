e3cf0f7a96ec06861b82b1aee69dc89a29acf09f
Countermatt <matthieu.pigaglio@uclouvain.be>
2024-09-26 11:00:52 +0200

    add drop for K1

--- simulator/run_bash.sh
+++ simulator/run_bash.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 # Run the first instance of run.sh with nohup
-nohup ./run.sh config/dasprotocol_K1.cfg > logs/dasprotocolSeedingStrategy1-k1.log &
+nohup ./run.sh config/dasprotocol_K1_10.cfg > logs/dasprotocolSeedingStrategy1-k1-10.log &
 # Wait for it to complete
 wait
 
 # Run the second instance of run.sh with nohup
-nohup ./run.sh config/dasprotocol_K2.cfg > logs/dasprotocolSeedingStrategy1-k2.log &
+nohup ./run.sh config/dasprotocol_K1_20.cfg > logs/dasprotocolSeedingStrategy1-k1-20.log &
 # Wait for it to complete
 wait
 
 # Run the third instance of run.sh with nohup
-nohup ./run.sh config/dasprotocol_K3.cfg > logs/dasprotocolSeedingStrategy1-k3.log &
+nohup ./run.sh config/dasprotocol_K1_30.cfg > logs/dasprotocolSeedingStrategy1-k1-30.log &
 # Wait for it to complete
 wait
 
 # Run the fourth instance of run.sh with nohup
-nohup ./run.sh config/dasprotocol_K4.cfg > logs/dasprotocolSeedingStrategy1-k4.log &
+nohup ./run.sh config/dasprotocol_K1_40.cfg > logs/dasprotocolSeedingStrategy1-k1-40.log &
 # Wait for it to complete
 wait
 
 # Run the fifth instance of run.sh with nohup
-nohup ./run.sh config/dasprotocol_K5.cfg > logs/dasprotocolSeedingStrategy1-k5.log &
+nohup ./run.sh config/dasprotocol_K1_50.cfg > logs/dasprotocolSeedingStrategy1-k1-50.log &
 # Wait for it to complete
 wait
 
