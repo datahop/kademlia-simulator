4ca396f52596f8b441a88cdb534eaf86894f17d0
Sergi Rene <sergi@dymension.xyz>
2025-01-19 17:35:20 +0100

    nonval random sampling

--- simulator/src/main/java/peersim/kademlia/das/DASProtocolNonValidator.java
+++ simulator/src/main/java/peersim/kademlia/das/DASProtocolNonValidator.java
@@ -59,7 +59,7 @@ public class DASProtocolNonValidator extends DASProtocol {
     validatorsContacted.clear();
     super.handleInitNewBlock(m, myPid);
     if (!isEvil) {
-      //  startRandomSampling();
+      startRandomSampling();
     }
   }
 
