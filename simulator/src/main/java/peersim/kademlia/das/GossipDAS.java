2f6737645628e46a06790a38b1751dfd3e2f95bb
Sergi Rene <sergi@dymension.xyz>
2025-02-23 08:55:40 +0100

    missing files

---
+++ simulator/src/main/java/peersim/kademlia/das/GossipDAS.java
@@ -0,0 +1,299 @@
+package peersim.kademlia.das;
+
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import peersim.config.Configuration;
+import peersim.core.CommonState;
+import peersim.core.Network;
+import peersim.core.Node;
+import peersim.edsim.EDProtocol;
+import peersim.edsim.EDSimulator;
+import peersim.kademlia.KademliaObserver;
+import peersim.kademlia.Message;
+import peersim.kademlia.SimpleEvent;
+import peersim.kademlia.Timeout;
+import peersim.kademlia.das.operations.SamplingOperation;
+import peersim.kademlia.gossipsub.GossipEvent;
+import peersim.kademlia.gossipsub.GossipSubProtocol;
+import peersim.transport.UnreliableTransport;
+
+public abstract class GossipDAS implements Cloneable, EDProtocol, GossipEvent {
+
+  protected static String prefix = null;
+  protected GossipSubProtocol gossipsub;
+  /** allow to call the service initializer only once */
+  protected static boolean _ALREADY_INSTALLED = false;
+
+  protected int[] row, column;
+  protected HashSet<BigInteger> kv;
+  protected BigInteger builderAddress;
+
+  protected Logger logger;
+  private boolean msgReport;
+  protected static final String PAR_TRANSPORT = "transport";
+  protected static final String PAR_GOSSIP = "gossipsub";
+  protected static final String PAR_MSG = "reportMsg";
+  protected TreeMap<Long, Message> sentMsg;
+  protected Block currentBlock;
+  protected SearchTable searchTable;
+  protected LinkedHashMap<Long, SamplingOperation> samplingOp;
+  private UnreliableTransport transport;
+  private int tid;
+  private long uploadInterfaceBusyUntil;
+  protected int protocolId;
+  protected boolean isBuilder, isValidator;
+
+  public GossipDAS(String prefix) {
+    GossipDAS.prefix = prefix;
+    _init();
+    msgReport = Configuration.getBoolean(prefix + "." + PAR_MSG, false);
+    sentMsg = new TreeMap<Long, Message>();
+    tid = Configuration.getPid(prefix + "." + PAR_TRANSPORT);
+
+    kv = new HashSet<>();
+    isBuilder = true;
+    isValidator = false;
+    samplingOp = new LinkedHashMap<Long, SamplingOperation>();
+  }
+
+  /**
+   * Replicate this object by returning an identical copy.<br>
+   * It is called by the initializer and do not fill any particular field.
+   *
+   * @return Object
+   */
+  public abstract Object clone();
+
+  /**
+   * This procedure is called only once and allow to inizialize the internal state of protocol.
+   * Every node shares the same configuration, so it is sufficient to call this routine once.
+   */
+  private void _init() {
+    // execute once
+    if (_ALREADY_INSTALLED) return;
+
+    _ALREADY_INSTALLED = true;
+  }
+
+  /**
+   * manage the peersim receiving of the events
+   *
+   * @param myNode Node
+   * @param myPid int
+   * @param event Object
+   */
+  public void processEvent(Node myNode, int myPid, Object event) {
+
+    Message m;
+    SimpleEvent s = (SimpleEvent) event;
+    if (s instanceof Message) {
+      m = (Message) event;
+      logger.info("Reporting " + msgReport + " " + m.getType() + " " + m.getSize());
+
+      if (msgReport
+          && (m.getType() == Message.MSG_GET_SAMPLE
+              || m.getType() == Message.MSG_GET_SAMPLE_RESPONSE
+              || m.getType() == Message.MSG_SEED_SAMPLE))
+        KademliaObserver.reportMsg(m, false, this.getNodeId());
+    }
+
+    switch (((SimpleEvent) event).getType()) {
+      case Message.MSG_INIT_NEW_BLOCK:
+        m = (Message) event;
+        handleInitNewBlock(m, myPid);
+        break;
+      case Message.MSG_GET_SAMPLE:
+        m = (Message) event;
+        handleGetSample(m, myPid);
+        break;
+      case Message.MSG_GET_SAMPLE_RESPONSE:
+        m = (Message) event;
+        handleGetSampleResponse(m, myPid);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  public void setBuilderAddress(BigInteger address) {
+    this.builderAddress = address;
+    searchTable.setBuilderAddress(address);
+  }
+
+  protected void handleInitNewBlock(Message m, int myPid) {
+    row = new int[currentBlock.getSize()];
+    column = new int[currentBlock.getSize()];
+    kv.clear();
+  }
+
+  protected abstract void handleGetSample(Message m, int myPid);
+
+  protected abstract void handleGetSampleResponse(Message m, int myPid);
+
+  /**
+   * sets the GossipSubProtocol instance can be used to run gossip operations
+   *
+   * @param prot GossipSubProtocol
+   */
+  public void setGossipProtocol(GossipSubProtocol prot) {
+    this.gossipsub = prot;
+    // this.logger = prot.getLogger();
+    this.gossipsub.setEventsCallback(this);
+    // Initialize the logger with the node ID as its name
+    this.logger = Logger.getLogger(this.getNodeId().toString());
+
+    // Disable the logger's parent handlers to avoid duplicate output
+    this.logger.setUseParentHandlers(false);
+
+    // Set the logger's level to WARNING
+    this.logger.setLevel(Level.WARNING);
+    // logger.setLevel(Level.ALL);
+
+    // Create a console handler for the logger
+    ConsoleHandler handler = new ConsoleHandler();
+    // Set the handler's formatter to a custom format that includes the time and logger name
+    handler.setFormatter(
+        new SimpleFormatter() {
+          private static final String format = "[%d][%s] %3$s %n";
+
+          @Override
+          public synchronized String format(LogRecord lr) {
+            return String.format(format, CommonState.getTime(), logger.getName(), lr.getMessage());
+          }
+        });
+    // Add the console handler to the logger
+    this.logger.addHandler(handler);
+    /*searchTable = new SearchTable(currentBlock, this.getKademliaId());*/
+  }
+
+  public BigInteger getNodeId() {
+    return this.gossipsub.getGossipNode().getId();
+  }
+
+  public void setSearchTable(SearchTable searchTable) {
+    this.searchTable = searchTable;
+  }
+
+  protected void reconstruct(Sample s) {
+    column[s.getColumn() - 1]++;
+    row[s.getRow() - 1]++;
+    if (column[s.getColumn() - 1] >= column.length / 2
+        && column[s.getColumn() - 1] != column.length) {
+      Sample[] samples = currentBlock.getSamplesByColumn(s.getColumn());
+      for (Sample sam : samples) {
+        kv.add(sam.getIdByRow());
+        // kv.add((BigInteger) sam.getIdByRow(), sam);
+        // kv.add((BigInteger) sam.getIdByColumn(), sam);
+      }
+      column[s.getColumn() - 1] = currentBlock.getSize();
+    }
+    if (row[s.getRow() - 1] >= row.length / 2 && row[s.getRow() - 1] != row.length) {
+      Sample[] samples = currentBlock.getSamplesByRow(s.getRow());
+      for (Sample sam : samples) {
+        kv.add(sam.getIdByRow());
+        // kv.add((BigInteger) sam.getIdByRow(), sam);
+        // kv.add((BigInteger) sam.getIdByColumn(), sam);
+      }
+      row[s.getRow() - 1] = currentBlock.getSize();
+    }
+  }
+
+  /**
+   * send a message with current transport layer and starting the timeout timer (wich is an event)
+   * if the message is a request
+   *
+   * @param m the message to send
+   * @param destId the Id of the destination node
+   * @param myPid the sender Pid
+   */
+  protected void sendMessage(Message m, BigInteger destId) {
+
+    // int destpid;
+    assert m.src != null;
+    assert m.dst != null;
+
+    Node src = this.gossipsub.getNode();
+    Node dest = GossipSubProtocol.nodeIdtoNode(destId, this.gossipsub.getProtocolID());
+
+    transport = (UnreliableTransport) (Network.prototype).getProtocol(tid);
+    if (msgReport
+        && (m.getType() == Message.MSG_GET_SAMPLE
+            || m.getType() == Message.MSG_GET_SAMPLE_RESPONSE
+            || m.getType() == Message.MSG_SEED_SAMPLE))
+      KademliaObserver.reportMsg(m, true, this.getNodeId());
+
+    if (m.getType() != Message.MSG_GET_SAMPLE_RESPONSE && m.getType() != Message.MSG_SEED_SAMPLE) {
+      transport.send(src, dest, m, this.protocolId);
+    } else {
+      // Send message taking into account the transmission delay and the availability of upload
+      // interface
+      // Timeout t = new Timeout(destId, m.id, m.operationId);
+      Sample[] samples;
+      if (m.getType() == Message.MSG_SEED_SAMPLE) {
+        SeedingSampleBody body = (SeedingSampleBody) m.body;
+        samples = (Sample[]) body.getsamplesList();
+      } else {
+        samples = (Sample[]) m.body;
+      }
+      // Sample[] samples = (Sample[]) m.body;
+      // Neighbour[] nghbrs = (Neighbour[]) m.value;
+      double msgSize = 0.0;
+      if (samples != null) msgSize = samples.length * KademliaCommonConfigDas.SAMPLE_SIZE;
+      long propagationLatency = transport.getLatency(src, dest);
+      // Add the transmission time of the message (upload)
+      double transDelay = 0.0;
+      if (this.isValidator) {
+        transDelay = 1000 * msgSize / KademliaCommonConfigDas.VALIDATOR_UPLOAD_RATE;
+      } else if (this.isBuilder) {
+        transDelay = 1000 * msgSize / KademliaCommonConfigDas.BUILDER_UPLOAD_RATE;
+      } else {
+        transDelay = 1000 * msgSize / KademliaCommonConfigDas.NON_VALIDATOR_UPLOAD_RATE;
+      }
+      // If the interface is busy, incorporate the additional delay
+      // also update the time when interface is available again
+      long timeNow = CommonState.getTime();
+      long latency = propagationLatency;
+      logger.info("Transmission propagationLatency " + latency);
+      latency += (long) transDelay; // truncated value
+      logger.info("Transmission total latency " + latency);
+      if (this.uploadInterfaceBusyUntil > timeNow) {
+        latency += this.uploadInterfaceBusyUntil - timeNow;
+        this.uploadInterfaceBusyUntil += (long) transDelay; // truncated value
+
+      } else {
+        this.uploadInterfaceBusyUntil = timeNow + (long) transDelay; // truncated value
+      }
+      logger.info("Transmission " + latency + " " + transDelay);
+      // add to sent msg
+      // this.sentMsg.put(m.id, m.timestamp);
+      EDSimulator.add(latency, m, dest, this.protocolId);
+    }
+
+    // Setup timeout
+    if (m.getType() == Message.MSG_GET_SAMPLE) { // is a request
+      Timeout t = new Timeout(destId, m.id, m.operationId);
+      long latency = transport.getLatency(src, dest);
+      logger.info("Send message added " + m.id + " " + latency + " " + destId);
+
+      // add to sent msg
+      this.sentMsg.put(m.id, m);
+      /// BigInteger[] samples = (Sample[]) m.body;
+
+      long timeout = latency * 2 * 4; // 4 RTT
+      if (timeout < 250) timeout = 250;
+      EDSimulator.add(timeout, t, src, this.protocolId); // set delay = 2*RTT
+    }
+  }
+
+  public void setProtocolId(int id) {
+    this.protocolId = id;
+  }
+}
---
+++ simulator/src/main/java/peersim/kademlia/das/operations/RandomSamplingOperationGossip.java
@@ -0,0 +1,185 @@
+package peersim.kademlia.das.operations;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import peersim.kademlia.das.Block;
+import peersim.kademlia.das.KademliaCommonConfigDas;
+import peersim.kademlia.das.Sample;
+import peersim.kademlia.gossipsub.GossipSubProtocol;
+
+/**
+ * This class represents a random sampling operation that collects samples from random nodes
+ *
+ * @author Sergi Rene
+ * @version 1.0
+ */
+public class RandomSamplingOperationGossip extends SamplingOperation {
+  BigInteger builderAddress;
+  /**
+   * default constructor
+   *
+   * @param srcNode Id of the node to find
+   * @param destNode Id of the node to find
+   * @param timestamp Id of the node to find
+   */
+  public RandomSamplingOperationGossip(
+      BigInteger srcNode,
+      BigInteger destNode,
+      long timestamp,
+      Block currentBlock,
+      boolean isValidator,
+      int numValidators,
+      BigInteger builderAddress) {
+    super(srcNode, destNode, timestamp, currentBlock, isValidator, numValidators);
+    this.builderAddress = builderAddress;
+    System.out.println("New RandomSampling ");
+    Sample[] randomSamples = currentBlock.getNRandomSamples(KademliaCommonConfigDas.N_SAMPLES);
+    for (Sample rs : randomSamples) {
+      FetchingSample s = new FetchingSample(rs);
+      samples.put(rs.getIdByRow(), s);
+    }
+  }
+
+  public void createNodes() {
+    for (BigInteger sample : samples.keySet()) {
+      boolean found = false;
+      if (!samples.get(sample).isDownloaded()) {
+        HashSet<BigInteger> validatorsBySampleRow =
+            GossipSubProtocol.getTable().getPeers("Row" + samples.get(sample).getRow());
+        HashSet<BigInteger> validatorsBySampleColumn =
+            GossipSubProtocol.getTable().getPeers("Column" + samples.get(sample).getColumn());
+        validatorsBySampleRow.remove(builderAddress);
+        validatorsBySampleColumn.remove(builderAddress);
+
+        List<BigInteger> validatorsBySample = new ArrayList<>();
+
+        validatorsBySample.addAll(validatorsBySampleRow);
+        validatorsBySample.addAll(validatorsBySampleColumn);
+
+        validatorsBySample.remove(builderAddress);
+
+        if (validatorsBySample != null && validatorsBySample.size() > 0) {
+          found = true;
+          for (BigInteger id : validatorsBySample) {
+            if (!nodes.containsKey(id)) {
+              nodes.put(id, new Node(id));
+              nodes.get(id).addSample(samples.get(sample));
+            } else {
+              nodes.get(id).addSample(samples.get(sample));
+            }
+          }
+        }
+        // if (!found) {
+        // throw new UnsupportedOperationException("no validators found");
+        // }
+      }
+    }
+  }
+
+  public boolean completed() {
+
+    boolean completed = true;
+    int failed = 0;
+    for (FetchingSample s : samples.values()) {
+      if (!s.isDownloaded()) {
+        failed++;
+        if (failed > KademliaCommonConfigDas.MAX_SAMPLING_FAILED) {
+          completed = false;
+          break;
+        }
+      }
+    }
+    return completed;
+  }
+
+  public BigInteger[] getSamples() {
+    List<BigInteger> result = new ArrayList<>();
+
+    for (FetchingSample sample : samples.values()) {
+      if (!sample.isDownloaded()) result.add(sample.getId());
+    }
+
+    return result.toArray(new BigInteger[0]);
+  }
+
+  protected void addExtraNodes() {}
+
+  public void elaborateResponse(Sample[] sam) {
+
+    for (Sample s : sam) {
+      if (samples.containsKey(s.getId()) /*|| samples.containsKey(s.getIdByColumn())*/) {
+        FetchingSample fs = samples.get(s.getId());
+        if (fs != null) {
+          if (!fs.isDownloaded()) {
+            samplesCount++;
+            fs.setDownloaded();
+          }
+        }
+      }
+    }
+    // System.out.println("Samples received " + samplesCount);
+  }
+
+  public void elaborateResponse(Sample[] sam, BigInteger node) {
+    // this.available_requests--;
+    // if (this.available_requests == 0) nodes.clear();
+    pendingNodes.remove(node);
+    Node n = nodes.get(node);
+    if (n != null) {
+      for (FetchingSample s : n.getSamples()) {
+        s.removeFetchingNode(n);
+        // s.setDownloaded();
+      }
+    }
+
+    if (sam != null) {
+      for (Sample s : sam) {
+        if (samples.containsKey(s.getId()) /*|| samples.containsKey(s.getIdByColumn())*/) {
+          FetchingSample fs = samples.get(s.getId());
+          // FetchingSample fs2 = samples.get(s.getIdByColumn());
+          if (fs != null) {
+            if (!fs.isDownloaded()) {
+              samplesCount++;
+              fs.setDownloaded();
+              fs.removeFetchingNode(nodes.get(node));
+            }
+          } /*else if(fs2!=null){
+               if(!fs2.isDownloaded()){
+            samplesCount++;
+            fs2.setDownloaded();
+            fs2.removeFetchingNode(nodes.get(node));
+               }
+
+             }*/
+        }
+      }
+    }
+    nodes.remove(node);
+    // askedNodes.add(node);
+  }
+
+  public Map<String, Object> toMap() {
+    // System.out.println("Mapping");
+    Map<String, Object> result = new HashMap<String, Object>();
+
+    result.put("id", this.operationId);
+    result.put("src", this.srcNode);
+    result.put("type", "RandomSamplingOperation");
+    result.put("messages", getMessagesString());
+    result.put("nodes_contacted", getMessages().size());
+    result.put("start", this.timestamp);
+    result.put("completion_time", this.stopTime);
+    result.put("hops", this.nrHops);
+    result.put("samples", this.samplesCount);
+    result.put("block_id", this.currentBlock.getBlockId());
+    if (isValidator) result.put("validator", "yes");
+    else result.put("validator", "no");
+    if (completed()) result.put("completed", "yes");
+    else result.put("completed", "no");
+    return result;
+  }
+}
