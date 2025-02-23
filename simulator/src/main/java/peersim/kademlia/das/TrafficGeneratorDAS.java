978c979259b3d2a062fb67a193c1f353638415d7
Sergi Rene <sergi@dymension.xyz>
2025-02-23 12:28:46 +0100

    missing files

---
+++ simulator/src/main/java/peersim/kademlia/das/TrafficGeneratorDAS.java
@@ -0,0 +1,95 @@
+package peersim.kademlia.das;
+
+import peersim.config.Configuration;
+import peersim.core.CommonState;
+import peersim.core.Control;
+import peersim.core.Network;
+import peersim.core.Node;
+import peersim.edsim.EDSimulator;
+import peersim.kademlia.Message;
+
+/**
+ * This control generates samples every 5 min that are stored in a single node (builder) and starts
+ * random sampling from the rest of the nodes In parallel, random lookups are started to start
+ * discovering nodes
+ *
+ * @author Sergi Rene
+ * @version 1.0
+ */
+
+// ______________________________________________________________________________________________
+public class TrafficGeneratorDAS implements Control {
+
+  /** MSPastry Protocol ID to act */
+  private final int kadpid;
+
+  private static final String PAR_KADPROT = "kadprotocol";
+
+  /** Mapping function for samples */
+  final String PAR_MAP_FN = "mapping_fn";
+
+  /** Number of sample copies stored per node */
+  final String PAR_NUM_COPIES = "sample_copy_per_node";
+
+  final String PAR_BLK_DIM_SIZE = "block_dim_size";
+
+  final String PAR_NUM_SAMPLES = "num_samples";
+
+  int mapfn;
+
+  Block b;
+  private long ID_GENERATOR = 0;
+  long lastTime = 0;
+
+  // ______________________________________________________________________________________________
+  public TrafficGeneratorDAS(String prefix) {
+    kadpid = Configuration.getPid(prefix + "." + PAR_KADPROT);
+
+    KademliaCommonConfigDas.MAPPING_FN = Configuration.getInt(prefix + "." + PAR_MAP_FN);
+    KademliaCommonConfigDas.NUM_SAMPLE_COPIES_PER_PEER =
+        Configuration.getInt(prefix + "." + PAR_NUM_COPIES);
+    KademliaCommonConfigDas.BLOCK_DIM_SIZE =
+        Configuration.getInt(
+            prefix + "." + PAR_BLK_DIM_SIZE, KademliaCommonConfigDas.BLOCK_DIM_SIZE);
+    KademliaCommonConfigDas.N_SAMPLES =
+        Configuration.getInt(prefix + "." + PAR_NUM_SAMPLES, KademliaCommonConfigDas.N_SAMPLES);
+  }
+
+  // ______________________________________________________________________________________________
+  /**
+   * generates a GET message for t1 key.
+   *
+   * @return Message
+   */
+  private Message generateNewBlockMessage(Block b) {
+
+    Message m = Message.makeInitNewBlock(b);
+    m.timestamp = CommonState.getTime();
+
+    return m;
+  }
+
+  // ______________________________________________________________________________________________
+  /**
+   * every call of this control generates and send a random find node message
+   *
+   * @return boolean
+   */
+  public boolean execute() {
+    Block b = new Block(KademliaCommonConfigDas.BLOCK_DIM_SIZE, ID_GENERATOR);
+
+    for (int i = 0; i < Network.size(); i++) {
+      Node n = Network.get(i);
+      if (n.isUp()) {
+        EDSimulator.add(0, generateNewBlockMessage(b), n, n.getDASProtocol().getDASProtocolID());
+      }
+    }
+    ID_GENERATOR++;
+    lastTime = CommonState.getTime();
+    return false;
+  }
+
+  // ______________________________________________________________________________________________
+
+} // End of class
+// ______________________________________________________________________________________________
